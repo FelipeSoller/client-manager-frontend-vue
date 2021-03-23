68581f7a7d69883c30bbc91f9890d36759ae17a5
Felipe Soller <soller@outlook.com>
2021-03-22 22:14:18 -0300

    feat: Adiciona NavBar com link das páginas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand order-md-last">Gerenciador de clientes</a>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to='/'>Lista de clientes</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to='/client-create'>Adicionar clientes</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to='/client-update'>Atualizar clientes</router-link>
+          </li>
+        </ul>
+    </nav>
+    <div class="container mt-5">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
