8c0e00a77aaf4e3eab3745b64ff495a1a342e435
Felipe Soller <soller@outlook.com>
2021-03-23 16:59:41 -0300

    feat: Configura métodos de requisições da API via HTTP

---
+++ src/services/ClientService.js
@@ -0,0 +1,44 @@
+import Api from './Api';
+
+export default {
+  async createNewClient(client) {
+    try {
+      const response = await Api().post('/clients', client);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getClients() {
+    try {
+      const response = await Api().get('/clients');
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getClientId(id) {
+    try {
+      const response = await Api().get(`/clients/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async updateClient(id) {
+    try {
+      const response = await Api().put(`/clients/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async deleteClient(id) {
+    try {
+      const response = await Api().delete(`/clients/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
